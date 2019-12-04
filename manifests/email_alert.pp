ca32759db076d3cba08208a69eda7ac14cf51094
Jesús Vélez Palacios <jvelez@tuenti.com>
2019-12-04 16:25:44 +0100

    _email_alert: add types, alert_format, alert_location, alert_do_not_delay, alert_do_not_group

--- manifests/email_alert.pp
+++ manifests/email_alert.pp
@@ -1,8 +1,12 @@
 # Wazuh App Copyright (C) 2019 Wazuh Inc. (License GPLv2)
 # Define an email alert
 define wazuh::email_alert(
-  $alert_email,
-  $alert_group = false
+  String                  $alert_email,
+  Optional[Array[String]] $alert_group = undef,
+  Enum['full', 'sms']     $alert_format = 'full',
+  Optional[String]        $alert_location = undef,
+  Boolean                 $alert_do_not_delay = false,
+  Boolean                 $alert_do_not_group = false,
 ) {
   require wazuh::params_manager
 
